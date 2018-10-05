a85192abd5ea3a208d621bad57deef72a59f273c
James Cherven <jcherven@mail.usf.edu>
2018-10-04 22:51:38 -0400

    Adds a demo for the Twitter API call, requires Node

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    consumer_key:         'AD5TEbpI9AsOZfCWeEh7VPfFv',
+    consumer_secret:      'DwzVgTRnzNFwayHj5b9Hd9DBOsDioPdqnQpmJDDvaCIlIvXwpZ',
+    app_only_auth:        true
+}
