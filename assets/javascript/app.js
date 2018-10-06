7f1c690fdd1e074bb21a464113d8c24453d68f33
James Cherven <jcherven@mail.usf.edu>
2018-10-06 18:32:19 -0400

    Gets input handling with index.html working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,27 @@
 
+$(document).ready(function() {
+  console.log('Document is ready');
+
+  // listen for an enter keypress and treat it like a button click
+  $('#input').keypress(function(event) {
+    if(event.which == 13) 
+      $('#submit-button').click();
+  });
+
+  // When input is submitted, call the handler
+  $('#submit-button').on('click', function() {
+    var $capturedInput = $('#input').val(); 
+
+    // Example output to the console
+    console.log( $capturedInput );
+
+    // Reset the input field after entry
+    $('#input').val('');
+  });
+  
+}); // End $(document).ready()
+
+/* Code for the Meaning Cloud call 
 
 var text = $("#text").val().trim();
 var apiKey= "d0c8e8c7a8e01ae93832dcadeab63133"
@@ -21,3 +44,4 @@ var settings = {
 $.ajax(settings).done(function (response) {
   console.log(response);
 });
+ */
